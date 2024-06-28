de8d7387c469040ea42aabc2373fcb2884393da7
Phantom <cachemoneysoftware@gmail.com>
2024-06-27 23:32:40 -0700

    Add src/ directory

--- main.rb
+++ main.rb
@@ -1,27 +1,4 @@
-require 'gosu'
+require './src/main_window'
 
-class GameWindow < Gosu::Window
-
-  GAME_WINDOW_WIDTH = 800
-  GAME_WINDOW_HEIGHT = 600
-  GAME_WINDOW_TITLE = 'Open Poker'
-  GAME_FONT_SIZE = 32
-  GAME_MESSAGE = 'Shuffle up and deal!'
-
-  def initialize
-    super GAME_WINDOW_WIDTH, GAME_WINDOW_HEIGHT
-    self.caption = GAME_WINDOW_TITLE
-
-    @message = Gosu::Font.new(GAME_FONT_SIZE)
-  end
-
-  def update
-  end
-
-  def draw
-    @message.draw_text(GAME_MESSAGE, 200, 200, 1)
-  end
-end
-
-window = GameWindow.new
+window = MainWindow.new
 window.show
---
+++ src/main_window.rb
@@ -0,0 +1,24 @@
+require 'gosu'
+
+class MainWindow < Gosu::Window
+
+  GAME_WINDOW_WIDTH = 800
+  GAME_WINDOW_HEIGHT = 600
+  GAME_WINDOW_TITLE = 'Open Poker'
+  GAME_FONT_SIZE = 32
+  GAME_MESSAGE = 'Shuffle up and deal!'
+
+  def initialize
+    super GAME_WINDOW_WIDTH, GAME_WINDOW_HEIGHT
+    self.caption = GAME_WINDOW_TITLE
+
+    @message = Gosu::Font.new(GAME_FONT_SIZE)
+  end
+
+  def update
+  end
+
+  def draw
+    @message.draw_text(GAME_MESSAGE, 200, 200, 1)
+  end
+end
