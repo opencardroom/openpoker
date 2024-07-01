c3f43fbafdb29aabdf276e9617b4de6974900832
Phantom <cachemoneysoftware@gmail.com>
2024-07-01 14:32:39 -0700

    Use Ruby unique object_id Integers as keys for ranks

--- src/card.rb
+++ src/card.rb
@@ -1,10 +1,10 @@
 class Card
 
   SUITS = %i[hearts diamonds clubs spades].freeze
-  RANKS = %i[1 2 3 4 5 6 7 8 9 10 11 12 13].freeze
-  READABLE_RANK = { '1': 'ace', '2': 'deuce', '3': '3', '4': '4', '5': '5',
-                    '6': '6', '7': '7', '8': '8', '9': '9', '10': '10',
-                    '11': 'jack', '12': 'queen', '13': 'king' }.freeze
+  RANKS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].freeze
+  READABLE_RANK = { 1 => 'ace', 2 => 'deuce', 3 => '3', 4 => '4', 5 => '5',
+                    6 => '6', 7 => '7', 8 => '8', 9 => '9', 10 => '10',
+                    11 => 'jack', 12 => 'queen', 13 => 'king' }.freeze
 
   attr_reader :suit, :rank
 
