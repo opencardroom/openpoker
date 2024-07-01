46c319aeeaf54dfb0de8146a3b30df805c34c399
Phantom <cachemoneysoftware@gmail.com>
2024-06-30 22:28:04 -0700

    Refactor: use symbols instead of strings, named parameters

--- src/card.rb
+++ src/card.rb
@@ -1,17 +1,17 @@
 class Card
 
-  SUITS = %w[Hearts Diamonds Clubs Spades]
-  RANKS = %w[2 3 4 5 6 7 8 9 10 Jack Queen King Ace]
+  SUITS = %i[hearts diamonds clubs spades].freeze
+  RANKS = %i[2 3 4 5 6 7 8 9 10 jack queen king ace].freeze
 
   attr_reader :suit, :rank
 
-  def initialize(suit, rank)
+  def initialize(suit:, rank:)
     @suit = suit
     @rank = rank
   end
 
   def to_s
-    "#{rank} of #{suit}"
+    "#{rank.capitalize} of #{suit.capitalize}"
   end
 
 end
