8869bbebc6dbaff3a42b52c16bd66ddbe8f96fd9
Phantom <cachemoneysoftware@gmail.com>
2024-06-30 23:36:24 -0700

    Add human readable card ranks

--- src/card.rb
+++ src/card.rb
@@ -1,7 +1,22 @@
 class Card
 
   SUITS = %i[hearts diamonds clubs spades].freeze
-  RANKS = %i[2 3 4 5 6 7 8 9 10 jack queen king ace].freeze
+  RANKS = %i[1 2 3 4 5 6 7 8 9 10 11 12 13].freeze
+  READABLE_RANK = {
+    '1': 'ace',
+    '2': 'deuce',
+    '3': '3',
+    '4': '4',
+    '5': '5',
+    '6': '6',
+    '7': '7',
+    '8': '8',
+    '9': '9',
+    '10': '10',
+    '11': 'jack',
+    '12': 'queen',
+    '13': 'king'
+  }.freeze
 
   attr_reader :suit, :rank
 
@@ -11,7 +26,11 @@ class Card
   end
 
   def to_s
-    "#{rank.capitalize} of #{suit.capitalize}"
+    "#{readable_rank.capitalize} of #{suit.capitalize}"
+  end
+
+  def readable_rank
+    READABLE_RANK[@rank]
   end
 
 end
